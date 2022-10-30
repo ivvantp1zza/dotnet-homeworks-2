f03fb5e872cb43524ef2b2c8a9e1b813cc72832d
ivvantp1zza <108880743+ivvantp1zza@users.noreply.github.com>
2022-10-30 22:15:02 +0300

    hw8

---
+++ Homework8/Hw8/Parser/ParseResult.cs
@@ -0,0 +1,19 @@
+﻿using Hw8.Calculator;
+
+namespace Hw8.Parser;
+
+public class ParseResult
+{
+    public double Value1 { get; }
+    public Operation Operation { get; }
+    public double Value2 { get; }
+    public string ErrorMessage { get; }
+
+    public ParseResult(double value1, Operation operation, double value2, string error = null)
+    {
+        Value1 = value1;
+        Operation = operation;
+        Value2 = value2;
+        ErrorMessage = error;
+    }
+}
---
+++ Homework8/Hw8/Parser/Parser.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+using Hw8.Calculator;
+
+namespace Hw8.Parser;
+
+public static class Parser
+{
+    public static ParseResult ParseCalcArguments(string[] args)
+    {
+        if (!double.TryParse(args[0], NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture, out var val1) ||
+            !double.TryParse(args[2], NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture, out var val2))
+            return new ParseResult(0, Operation.Invalid, 0, Messages.InvalidNumberMessage);
+        var operation = ParseOperation(args[1]);
+        return operation switch
+        {
+            Operation.Invalid => new ParseResult(val1, operation, val2, Messages.InvalidOperationMessage),
+            Operation.Divide when val2 < double.Epsilon => new ParseResult(0, Operation.Invalid, 0,
+                Messages.DivisionByZeroMessage),
+            _ => new ParseResult(val1, operation, val2)
+        };
+    }
+    
+    private static Operation ParseOperation(string arg)
+    {
+        return arg.ToLower() switch
+        {
+            "plus" => Operation.Plus,
+            "minus" => Operation.Minus,
+            "multiply" => Operation.Multiply,
+            "divide" => Operation.Divide,
+            _ => Operation.Invalid
+        };
+    }
+}
