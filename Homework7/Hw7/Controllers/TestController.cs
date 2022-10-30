c086a7ea6b015bd49ac96a25bfd05c537bd75794
ivvantp1zza <108880743+ivvantp1zza@users.noreply.github.com>
2022-10-30 17:03:11 +0300

    hw7

---
+++ Homework7/Hw7/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using Hw7.Models;
+using Hw7.Models.ForTests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hw7.Controllers;
+
+public class TestController : Controller
+{
+    // GET
+    [HttpGet]
+    public IActionResult TestModel()
+    {
+        return View();
+    }
+    
+    [HttpPost]
+    public IActionResult TestModel(TestModel testModel)
+    {
+        return View(testModel);
+    }
+}
