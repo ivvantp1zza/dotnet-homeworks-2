eee60ee9a20e9bc1316fa2694e4674cbdd048d62
ivvantp1zza <108880743+ivvantp1zza@users.noreply.github.com>
2022-11-14 13:29:16 +0300

    hw9 add

---
+++ Homework9/Hw9/Services/MathCalculator/TokenParser/Token.cs
@@ -0,0 +1,38 @@
+﻿namespace Hw9.Services.MathCalculator.TokenParser;
+
+public class Token
+{
+    public TokenType Type { get; }
+    
+    public string Value { get; }
+
+    public Token(TokenType type, string value = null)
+    {
+        Type = type;
+        Value = value;
+    }
+    
+    public bool IsNumber() => Type == TokenType.Number;
+    public bool IsBinaryOperator() =>
+        Type switch
+        {
+            TokenType.Plus => true,
+            TokenType.Minus => true,
+            TokenType.Multiply => true,
+            TokenType.Divide => true,
+            _ => false
+        };
+    public bool IsNegate() => Type == TokenType.Negate;
+
+    public bool IsOperator() => IsBinaryOperator() || IsNegate();
+    
+    public int GetRank() =>
+        Type switch
+        {
+            TokenType.OpenBracket or TokenType.CloseBracket => -1,
+            TokenType.Plus or TokenType.Minus => 0,
+            TokenType.Multiply or TokenType.Divide => 1,
+            TokenType.Negate => 2,
+            _ => throw new ArgumentOutOfRangeException()
+        };
+}
