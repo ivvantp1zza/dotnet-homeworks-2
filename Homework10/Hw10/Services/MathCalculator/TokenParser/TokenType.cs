177f1d022eef8446b11b01108abcecc6f32222fe
ivvantp1zza <108880743+ivvantp1zza@users.noreply.github.com>
2022-11-15 22:22:23 +0300

    hw10 add

---
+++ Homework10/Hw10/Services/MathCalculator/TokenParser/TokenType.cs
@@ -0,0 +1,13 @@
+﻿namespace Hw10.Services.MathCalculator.TokenParser;
+
+public enum TokenType
+{
+    Number,
+    Plus,
+    Minus,
+    Multiply,
+    Divide,
+    Negate,
+    OpenBracket,
+    CloseBracket
+}
---
+++ Homework10/Hw10/Services/Visitor/MyExpressionVisitor.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Linq.Expressions;
+using Hw10.ErrorMessages;
+
+namespace Hw10.Services.Visitor;
+
+[ExcludeFromCodeCoverage]
+public class MyExpressionVisitor : ExpressionVisitor
+{
+    protected override Expression VisitBinary(BinaryExpression root)
+    {
+        var result = CompileAsync(root.Left, root.Right).Result;
+        
+        return root.NodeType switch
+        {
+            ExpressionType.Add => Expression.Add(Expression.Constant(result[0]), Expression.Constant(result[1])),
+            ExpressionType.Subtract => Expression.Subtract(Expression.Constant(result[0]), Expression.Constant(result[1])),
+            ExpressionType.Multiply => Expression.Multiply(Expression.Constant(result[0]), Expression.Constant(result[1])),
+            _ => result[1] < double.Epsilon
+                ? throw new Exception(MathErrorMessager.DivisionByZero)
+                : Expression.Divide(Expression.Constant(result[0]), Expression.Constant(result[1]))
+        };
+    }
+    public Task<Expression> MyVisit(Expression expr) =>
+        Task.Run(() => base.Visit(expr));
+
+    private async Task<double[]> CompileAsync(Expression left, Expression right)
+    {
+        var t = Task.Run(async() =>
+        {
+            await Task.Delay(3000);
+            return Expression.Lambda<Func<double>>(left).Compile().Invoke();
+        });
+        var t1 = Task.Run(async() =>
+        {
+            await Task.Delay(3000);
+            return Expression.Lambda<Func<double>>(right).Compile().Invoke();
+        });
+        return await Task.WhenAll(t, t1);
+    }
+}
